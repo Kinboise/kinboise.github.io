0cfe8cdf3c04e6c12c2261ef7acdbd40e0e900fa
Kinboise <zsimghuadsjing@163.com>
2021-04-03 16:34:30 +0800

    e bise mer

--- Xef/tuc.js
+++
@@ -1,9 +0,0 @@
-function tuc(imgid) {
-    var img = document.getElementById(imgid);
-    if (img.style.display != 'none') {
-        img.style.display = 'none';
-    }
-    else {
-        img.style.display = 'block';
-    }
-}
