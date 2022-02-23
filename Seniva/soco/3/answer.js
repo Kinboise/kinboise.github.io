69af6d2c2c713b6cadc61eb6210f88904d3fb4da
Kinboise <zsimghuadsjing@163.com>
2022-02-23 23:19:40 +0800

    202202
    更新词典；第三课；美化首页

---
+++ Seniva/soco/3/answer.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    // Create quiz instances for each quiz and add them to the quizzes map.
+    // The key is the ID of the quiz element, same as what we pass to the Quiz object as the first argument.
+    quizzes['quiz-1'] = new Quiz('quiz-1', [
+        '狗跳。',
+        'bake flig.'
+    ]);
+
+    quizzes['quiz-2'] = new Quiz('quiz-2', [
+        'gale cice lip.'
+    ]);
+
+    quizzes['quiz-3'] = new Quiz('quiz-3', [
+        'li sed.',
+        'di lapre gupte pav.',
+        'li flige ma flig.',
+        'a'
+    ])
+};
+
